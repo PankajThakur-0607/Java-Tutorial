f0bf6cb1e0dc7fccfdc0c6604866b24bfaa31444
Pankaj Thakur <pankajothakur456@gmail.com>
2025-01-18 21:17:18 +0530

    another Linear Search add

--- Recursion/_07_recurison.java
+++ Recursion/_07_recurison.java
@@ -73,6 +73,24 @@ public class _07_recurison {
         return linearSearch_V4_AllIndexes(arr, index + 1, target, ans);
     }
 
+
+    // O(N) && O(N) + O(N)
+    public static ArrayList<Integer> linearSearch_V5_AllIndexes(int arr[], int index, int target) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length) {
+            return list;
+        }
+
+        if (arr[index] == target) {
+            list.add(index);
+        }
+
+        ArrayList<Integer> ansFromBelowCalls = linearSearch_V5_AllIndexes(arr, index + 1, target);
+        list.addAll(ansFromBelowCalls);
+
+        return list;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 3, 4, 3, 5, 7 };
         System.out.println("Array is Sorted : " + isSorted_V1(arr, 0, arr.length - 1));
@@ -82,6 +100,7 @@ public class _07_recurison {
         System.out.println("Array Found : " + linearSearch_V2(arr, arr.length - 1, 9));
         System.out.println("Array Found : " + linearSearch_V3(arr, 0, 9));
         System.out.println("Array Found : " + linearSearch_V4_AllIndexes(arr, 0, 3, new ArrayList<>()));
+        System.out.println("Array Found : " + linearSearch_V5_AllIndexes(arr, 0, 3));
 
     }
 }
