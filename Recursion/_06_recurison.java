7e4d093e085bbd7edb4c174d64370b86f01ad7dd
Pankaj Thakur <pankajothakur456@gmail.com>
2025-01-18 22:15:33 +0530

    Add the search Rotated Array Problem

--- Recursion/_06_recurison.java
+++ Recursion/_06_recurison.java
@@ -30,6 +30,32 @@ public class _06_recurison {
 
     }
 
+    // TC -- > O(log N) && SC O(N)
+    public static int searchRotatedArray(int arr[], int target, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[si] <= arr[mid]) {
+
+            if (target > arr[si] && target < arr[mid]) {
+                return searchRotatedArray(arr, target, si, mid - 1);
+            } else {
+                return searchRotatedArray(arr, target, mid + 1, ei);
+            }
+        }
+
+        if (target > arr[mid] && target < arr[ei]) {
+            return searchRotatedArray(arr, target, mid + 1, ei);
+        }
+        return searchRotatedArray(arr, target, si, mid - 1);
+    }
+
     public static void main(String[] args) {
         int n = 30204;
         countZeroes_V1(n);
@@ -37,5 +63,7 @@ public class _06_recurison {
 
         System.out.println(countZeroes_V2(n, 0));
 
+        int arr[] = { 5, 6, 7, 8, 9, 1, 2, 3 };
+        System.out.println("Fount at Index : " + searchRotatedArray(arr, 4, 0, arr.length - 1));
     }
 }
