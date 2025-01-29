e8b71169c764660949a4fc11486c633653e3014d
Pankaj Thakur <pankajothakur456@gmail.com>
2025-01-29 21:19:42 +0530

    add the new Recursion Insertion sort

--- Recursion/EasyLevelProblems/InsertionSort.java
+++ Recursion/EasyLevelProblems/InsertionSort.java
@@ -9,7 +9,7 @@ public class InsertionSort {
             int curr = arr[i];
             int prev = i - 1;
 
-            while (prev != -1 && arr[prev] > curr) {
+            while (prev >= 0 && arr[prev] > curr) {
                 arr[prev + 1] = arr[prev];
                 prev--;
             }
@@ -18,15 +18,28 @@ public class InsertionSort {
         }
     }
 
-    public static void insertionSort_V2(int arr[]){
-        
+    public static void insertionSort_V2(int arr[], int n) {
+        if (n <= 1) {
+            return;
+        }
+
+        insertionSort_V2(arr, n - 1);
+        int lastElement = arr[n - 1];
+        int prevIdx = n - 2;
+
+        while (prevIdx != -1 && arr[prevIdx] > lastElement) {
+            arr[prevIdx + 1] = arr[prevIdx];
+            prevIdx--;
+        }
+        arr[prevIdx + 1] = lastElement;
     }
 
     public static void main(String[] args) {
         int arr[] = { 5, 2, 1, 3, 4 };
+        System.out.println(Arrays.toString(arr));
+        // insertSort(arr);
+        insertionSort_V2(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
 
-        System.out.println(Arrays.toString(arr));
-        insertSort(arr);
-        System.out.println(Arrays.toString(arr));
     }
 }
