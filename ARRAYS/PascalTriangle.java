cba6e7833e7fa37fd72dc2fcbefd6c0edfd92d4e
Pankaj Thakur <pankajothakur456@gmail.com>
2025-04-10 14:12:58 +0530

    update Pascal triangle

--- ARRAYS/PascalTriangle.java
+++ ARRAYS/PascalTriangle.java
@@ -3,102 +3,162 @@ import java.util.List;
 
 public class PascalTriangle {
 
+    // Tc -> O(N) && Sc -> O(N)
     public static int factorial(int n) {
         if (n <= 1) {
-            return 1;
+            return n;
         }
 
         return n * factorial(n - 1);
     }
 
-    // function to get the element from the pascal traingle
+    // O(2*row) && O(row)
+    public static int getElementValue(int row, int col) {
+        if (col == 0 || col == row) {
+            return 1;
 
-    static class GetPascalValues {
+        }
+        long numerator = factorial(row);
+        long denominator = factorial(row - col) * factorial(col);
+        long nCr = (numerator / denominator);
+        return (int) nCr;
+    }
 
-        public long getElm_V1(int row, int col) {
-            int numerator = factorial(row);
-            int denominator = factorial(col) * factorial(row - col);
-            long nCr = numerator / denominator;
-            return nCr;
+    // O(col) && O(1)
+    public static int getElementValue_V2(int row, int col) {
+        int result = 1;
+
+        for (int i = 0; i < col; i++) {
+            result = result * (row - i);
+            result = result / (i + 1);
         }
 
-        // O(R)
-        public long getElm_V2(int row, int col) {
+        return result;
+    }
 
-            long result = 1;
-            for (int i = 0; i < col; i++) {
-                result = result * (row - i);
-                result = result / (i + 1);
-            }
+    // Function to print the Pascal's triangle
 
-            return result;
+    // O(row ^ 2) && O(row)
+    public static List<Integer> getPascalTriangleRow(int row) {
+        List<Integer> pascalRow = new ArrayList<>();
+
+        for (int i = 0; i <= row; i++) {
+            pascalRow.add(getElementValue(row, i));
         }
 
-        public List<Long> getRowInTriangle_V1(int row) {
-            List<Long> ans = new ArrayList<>();
-            for (int i = 0; i <= row; i++) {
-                ans.add(getElm_V2(row, i));
-            }
+        return pascalRow;
+    }
 
-            return ans;
+    // O(row * col) && O(row)
+    public static List<Integer> getPascalTriangleRow_V2(int row) {
+        List<Integer> pascalRow = new ArrayList<>();
+
+        for (int i = 0; i <= row; i++) {
+            pascalRow.add(getElementValue_V2(row, i));
         }
 
-        // O(N)
-        public List<Long> getRowInTraingle_V2(int row) {
-            List<Long> ans = new ArrayList<>();
-            ans.add((long) 1);
-            long result = 1;
-            for (int i = 1; i < row; i++) {
-                result = result * (row - i);
-                result = result / i;
-                ans.add(result);
-            }
+        return pascalRow;
+    }
 
-            return ans;
+    public static List<Integer> getPascalTriangleRow_V3(int row) {
+        List<Integer> pascalRow = new ArrayList<>();
+        int result = 1;
+        pascalRow.add(1);
+        for (int i = 1; i < row; i++) {
+            result = result * (row - i);
+            result = result / i;
+            pascalRow.add(result);
         }
 
-        public List<List<Long>> printCompleteTiangle_V1(int rows) {
-            List<List<Long>> ans = new ArrayList<>();
-            for (int i = 0; i < rows; i++) {
-                ans.add(new ArrayList<>());
-            }
+        return pascalRow;
+    }
 
-            // O(r * i * j) && O(n(n +1) / 2)
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j <= i; j++) {
-                    ans.get(i).add(getElm_V2(i, j));
+    // Function to print the Pascal's triangle
+
+    // O(n^3) && O(row ^2)
+    public static List<List<Integer>> getPascalTriangle(int n) {
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            pascalTriangle.add(new ArrayList<>(getPascalTriangleRow(i)));
+        }
+
+        return pascalTriangle;
+    }
+
+    // O(n * row * col) && O(row * col)
+    public static List<List<Integer>> getPascalTriangle_V2(int n) {
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            pascalTriangle.add(new ArrayList<>(getPascalTriangleRow_V2(i)));
+        }
+
+        return pascalTriangle;
+    }
+
+    public static List<List<Integer>> getPascalTriangle_V3(int n) {
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            pascalTriangle.add(new ArrayList<>(getPascalTriangleRow_V3(i)));
+        }
+
+        return pascalTriangle;
+    }
+
+    // O(N^2) && O(N^2)
+    public static List<List<Integer>> getPascalTriangle_V4(int n) {
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+
+        for (int R = 0; R < n; R++) {
+            List<Integer> pascalRow = new ArrayList<>();
+
+            for (int C = 0; C <= R; C++) {
+                if (C == 0 || C == R) {
+                    pascalRow.add(1);
+                } else {
+                    int val = pascalTriangle.get(R - 1).get(C) + pascalTriangle.get(R - 1).get(C - 1);
+                    pascalRow.add(val);
                 }
             }
 
-            return ans;
+            pascalTriangle.add(pascalRow);
+
         }
 
-        public List<List<Long>> printCompleteTiangle_V2(int rows) {
-            List<List<Long>> ans = new ArrayList<>();
-
-            // O(N * N)
-            for (int i = 1; i <= rows; i++) {
-                ans.add(new ArrayList<>(getRowInTraingle_V2(i)));
-            }
-
-            return ans;
-        }
-
+        return pascalTriangle;
     }
 
     public static void main(String[] args) {
+        int row = 6;
+        int col = 3;
 
-        int row = 4, col = 2;
-        // System.out.println(factorial(6));
+        System.out.println("Value at row " + row + " and column " + col + " is: " +
+        getElementValue(row - 1, col));
 
-        GetPascalValues obj = new GetPascalValues();
-        // System.out.println(obj.getElm_V1(row - 1, col - 1));
-        // System.out.println(obj.getElm_V2(row - 1, col - 1));
+        // System.out.println(getPascalTriangleRow(row - 1));
 
-        // System.out.println(obj.getRowInTriangle_V1(row - 1));
-        System.out.println(obj.getRowInTraingle_V2(row));
+        // System.out.println(getPascalTriangle(6));
 
-        System.out.println(obj.printCompleteTiangle_V1(4));
-        System.out.println(obj.printCompleteTiangle_V2(row));
+        // System.out.println("---------------------");
+        // System.out.println("Value at row " + row + " and column " + col + " is: " +
+        // getElementValue_V2(row - 1, col));
+
+        // System.out.println(getPascalTriangleRow_V2(row - 1));
+        // System.out.println(getPascalTriangle_V2(6));
+
+        // System.out.println("---------------------");
+        // System.out.println("Value at row " + row + " and column " + col + " is: " +
+        // getElementValue_V2(row - 1, col));
+        // System.out.println(getPascalTriangleRow_V3(row - 1));
+        // System.out.println(getPascalTriangle_V3(6));
+
+        // System.out.println("---------------------");
+        // System.out.println("Value at row " + row + " and column " + col + " is: " +
+        // getElementValue_V2(row - 1, col));
+        // System.out.println(getPascalTriangleRow_V3(row - 1));
+        System.out.println(getPascalTriangle_V4(6));
+
     }
 }
